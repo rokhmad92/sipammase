268b7235114aa8e19ab37e5bb1763ee76b4c532e
Rokhmad Yulian <rokhmad92@gmail.com>
2023-05-20 06:26:37 +0700

    Start Project

---
+++ resources/views/partviewAdmin/footer.blade.php
@@ -0,0 +1,35 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.2.0
+    </div>
+</footer>
+
+{{-- template script --}}
+<script src="{{ asset('template') }}/plugins/jquery/jquery.min.js"></script>
+<script src="{{ asset('template') }}/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script>
+    $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('template') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('template') }}/plugins/chart.js/Chart.min.js"></script>
+<script src="{{ asset('template') }}/plugins/sparklines/sparkline.js"></script>
+<script src="{{ asset('template') }}/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="{{ asset('template') }}/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="{{ asset('template') }}/plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="{{ asset('template') }}/plugins/moment/moment.min.js"></script>
+<script src="{{ asset('template') }}/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="{{ asset('template') }}/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="{{ asset('template') }}/plugins/summernote/summernote-bs4.min.js"></script>
+<script src="{{ asset('template') }}/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="{{ asset('template') }}/dist/js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{ asset('template') }}/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{ asset('template') }}/dist/js/pages/dashboard.js"></script>
+
+{{-- my script --}}
+</body>
+</html>
---
+++ resources/views/partviewAdmin/sidebar.blade.php
@@ -0,0 +1,87 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Sidebar -->
+        <div class="sidebar">
+            <!-- User panel) -->
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="image">
+                    <img src="{{ asset('images') }}/logo.png" class="img-circle elevation-2" alt="User Image">
+                </div>
+                <div class="info">
+                    <a href="#" class="d-block">Admin</a>
+                </div>
+            </div>
+
+            <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>Beranda</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-book"></i>
+                                <p>Pengajuan Harmonisasi</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-search"></i>
+                                <p>Administrasi Dan Analisis Konsepsi</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-receipt"></i>
+                                <p>Rapat Harmonisasi</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-box"></i>
+                                <p class="text-sm">Penyampaian Harmonisasi</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-chart-line"></i>
+                                <p>Grafik Harmonisasi</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-calendar-alt"></i>
+                                <p>Agenda Rapat</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-database"></i>
+                                <p>Master Data</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-user-plus"></i>
+                                <p>Profile</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-user"></i>
+                                <p>User</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-door-open"></i>
+                                <p>Logout</p>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+        </div>
+    <!-- /.sidebar -->
+</aside>
